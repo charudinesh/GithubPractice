045aa3fd96ad4efd35070e23aa0dfbe7c332531f
charudinesh <charu.uppal@yahoo.com>
2016-07-28 21:59:12 +0530

    First commit

---
+++ WebdriverProject/src/testscripts/NavigatetoRyanair.java
@@ -0,0 +1,13 @@
+package testscripts;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class NavigatetoRyanair {
+
+	public static void main(String[] args) {
+		WebDriver driver = new FirefoxDriver();
+		driver.get("https://sit-aem.ryanair.com/gb/en");
+	}
+
+}
